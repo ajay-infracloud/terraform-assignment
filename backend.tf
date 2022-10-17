ff32b2f2a1932245178299350b46d51646e57348
Ajay Joshi <ajay.joshi@infracloud.io>
2022-10-17 10:14:58 +0530

    configure s3 as backend

--- backend.tf
+++ backend.tf
@@ -1,10 +1,8 @@
-# terraform {
-#   required_version = ">= 0.13.0"
-#   backend "s3" {
-#     bucket = "infracloud-test-state"
-#     key    = "${var.stack_name}/terraform.tfstate"
-#     region = "eu-west-2"
-#     access_key = "AKIA4AOTU3Y4MVF4E2GA"
-#     secret_key = "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"
-#   }
-# }
+terraform {
+  required_version = ">= 0.13.0"
+  backend "s3" {
+    bucket = "infracloud-test-state"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
